4a11d1f302f57422ab4f48984037e0a4fbf6142c
猫背 <nekozezaku@gmail.com>
2018-01-19 07:38:33 +0900

    てき が あらわれた !!
    画像表示　プログレスバー表示

--- setting.py
+++ setting.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), 'a.env')
 load_dotenv(dotenv_path)
 
 Github_User = os.environ.get("Github_User")
